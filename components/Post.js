35bd8788bfa11a6147251972642c1659cf7a71c0
nesh <markmunene.nesh@gmail.com>
2021-12-15 02:19:23 +0300

    remove position absolute in timestamp paragraph

--- components/Post.js
+++ components/Post.js
@@ -7,13 +7,13 @@ function Post({post}) {
     
     return (
         <Link href={`/${post.id}`}>
-        <div className="bg-white relative px-6 py-4 group rounded-lg cursor-pointer ">
+        <div className="bg-white relative px-6 py-2 group rounded-lg cursor-pointer ">
 
 <h1 className=" text-xl md:text-2xl font-semibold group-hover:underline ">{post?.title}</h1>
-<p className="text-gray-800 mt-2 mb-4  line-clamp-2 ">{post?.content}</p>
+<p className="text-gray-800 my-2  line-clamp-2 ">{post?.content}</p>
 
 
-<p className='text-sm text-gray-700 absolute bottom-1 left-0 pl-6 '>Asked <Moment fromNow>{post?.timestamp}</Moment> by {post?.firstName}</p>
+<p className='text-sm text-gray-700 '>Asked <Moment fromNow>{post?.timestamp}</Moment> by {post?.firstName}</p>
 
             
         </div>
